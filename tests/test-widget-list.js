dc742748508456f3fd73a36eca9ef71510d93c07
Andrew Sutherland <asutherland@asutherland.org>
2010-06-28 04:01:39 -0700

    end of day status commit

---
+++ tests/test-widget-list.js
@@ -0,0 +1,13 @@
+/**
+ * Test widget list functionality.
+ **/
+
+var Pages = require("page-worker");
+var wmsy = require("wmsy/wmsy");
+
+/**
+ *
+ */
+exports.testEnsureVisible = function testEnsureVisible(test) {
+
+};
