bf3ddee5c13389d75a683d96817973ced5f211b3
Andrew Sutherland <asutherland@asutherland.org>
2010-06-28 19:53:19 -0700

    implement click-to-focus; tests for recent focus logic in process.

--- tests/test-widget-list.js
+++ tests/test-widget-list.js
@@ -1,13 +1,29 @@
 /**
- * Test widget list functionality.
+ * Test widget list functionality not covered elsewhere.
  **/
 
 var Pages = require("page-worker");
 var wmsy = require("wmsy/wmsy");
 
 /**
- *
+ * Make sure ensureVisible with a straightforward list is able to scroll
+ *  horizontally/vertically.
  */
-exports.testEnsureVisible = function testEnsureVisible(test) {
+function baseEnsureVisibleSimple(test, aVertical) {
+  var vertString = aVertical ? "vertical" : "horizontal";
+
+  var wy = new wmsy.WmsyDomain({id: "wl-ensurevisible-" + vertString,
+                                domain: "wl-ensurevisible-" + vertString});
+
+};
+
+exports.testEnsureVisibleSimple = function testEnsureVisibleSimple(test) {
+
+};
+
+/**
+ * Make sure ensureVisible handles
+ */
+exports.testEnsureVisibleComplex = function testEnsureVisibleComplex(test) {
 
 };
