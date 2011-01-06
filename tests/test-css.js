d3ae1a08d2df2499603b38d7f03c3ab94a26c6aa
Andrew Sutherland <asutherland@asutherland.org>
2011-01-06 07:24:00 -0800

    - add maximum width/height for popup size descriptor - change CSS generation to use child selector rather than descendent - some minor doc changes

--- tests/test-css.js
+++ tests/test-css.js
@@ -44,7 +44,7 @@ exports.testCssBasics = function testCssBasics(test) {
   var cssMapifications = [
     [{root: "a: b;"}, ".z-root {a: b;}"],
     [{foo: "a: b;"}, ".z-foo {a: b;}"],
-    [{root: {":hover": {foo: "a: b;"}}}, ".z-root:hover .z-foo {a: b;}"],
+    [{root: {":hover": {foo: "a: b;"}}}, ".z-root:hover > .z-foo {a: b;}"],
     [{foo: {":hover": "a: b;"}}, ".z-foo:hover {a: b;}"],
   ];
   const cssPrefix = "z-";
